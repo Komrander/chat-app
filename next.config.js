68fa433c362160c4170aa3e645bab387ed37e45d
Komrander <87586480+Komrander@users.noreply.github.com>
2023-02-23 21:19:15 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-        NEXTAUTH_SECRET: 'a0332deb5369b6b1ed57187841fe73e5',
-    },
 };
 
 module.exports = nextConfig
