a01559fc54c2b83dab33ebadf07879896bff4fe7
Pícl Matouš <picl.m@sosceskybrod.cz>
2023-03-15 12:02:16 +0100

    updated sidenav

--- components/app/sidenav.js
+++ components/app/sidenav.js
@@ -13,34 +13,44 @@ export default function Sidenav() {
 
     return (
         <div className={styles.container}>
-            <div>
-                <div className={styles.titleContainer}>
-                    <h1 className={styles.title}>Chats</h1>
-                    <Button title="Add" image="icons/plus.png" imageDark="icons/plusDark.png"/>
-                </div>
-                <div className={styles.navButton}>
-                    <Profile style="small"/>
-                    <div className={styles.navButtonContainer}>
-                        <div className={styles.navButtonHeader}>
-                            <h1 className={styles.navButtonName}>friend 1</h1>
-                            <h1 className={styles.navButtonTime}>2 minutes ago</h1>
-                        </div>
-                        <div className={styles.navButtonContent}>
-                            <h1 className={styles.navButtonMessageUnread}>Supporting line text lorem ipsum dolor sit amet, consectetur</h1>
-                            <div className={styles.navButtonNotification}>1</div>
-                        </div>
+            <div className={styles.titleContainer}>
+                <h1 className={styles.title}>Chats</h1>
+                <Button title="Add" image="icons/plus.png" imageDark="icons/plusDark.png"/>
+            </div>
+            <div className={styles.navButton}>
+                <Profile style="small"/>
+                <div className={styles.navButtonContainer}>
+                    <div className={styles.navButtonHeader}>
+                        <h1 className={styles.navButtonNameHighlight}>friend 1</h1>
+                        <h1 className={styles.navButtonTimeHighlight}>2 minutes ago</h1>
+                    </div>
+                    <div className={styles.navButtonContent}>
+                        <h1 className={styles.navButtonMessageHighlight}>Supporting line text lorem ipsum dolor sit amet, consectetur</h1>
+                        <div className={styles.navButtonNotification}>1</div>
                     </div>
                 </div>
-                <div className={styles.navButton}>
-                    <Profile style="small"/>
-                    <div className={styles.navButtonContainer}>
-                        <div className={styles.navButtonHeader}>
-                            <h1 className={styles.navButtonName}>friend 1</h1>
-                            <h1 className={styles.navButtonTime}>40 minutes ago</h1>
-                        </div>
-                        <div className={styles.navButtonContent}>
-                            <h1 className={styles.navButtonMessage}>Supporting line text lorem ipsum dolor sit amet, consectetur</h1>
-                        </div>
+            </div>
+            <div className={styles.navButton}>
+                <Profile style="small"/>
+                <div className={styles.navButtonContainer}>
+                    <div className={styles.navButtonHeader}>
+                        <h1 className={styles.navButtonName}>friend 1</h1>
+                        <h1 className={styles.navButtonTime}>40 minutes ago</h1>
+                    </div>
+                    <div className={styles.navButtonContent}>
+                        <h1 className={styles.navButtonMessage}>You: bye</h1>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.navButtonHighlight}>
+                <Profile style="small"/>
+                <div className={styles.navButtonContainer}>
+                    <div className={styles.navButtonHeader}>
+                        <h1 className={styles.navButtonNameHighlight}>group 1</h1>
+                        <h1 className={styles.navButtonTimeHighlight}>4 hours ago</h1>
+                    </div>
+                    <div className={styles.navButtonContent}>
+                        <h1 className={styles.navButtonMessageHighlight}>Supporting line text lorem ipsum dolor sit amet, consectetur</h1>
                     </div>
                 </div>
             </div>
