3606a45f1cbd63c9cac17316afc8dae57c8a5a7f
Komrander x <matous.picl@seznam.cz>
2023-03-27 04:09:42 +0200

    updated login and misc

--- components/app/header.js
+++ components/app/header.js
@@ -1,14 +1,10 @@
-import { signOut } from "next-auth/react";
 import styles from './header.module.css';
-
-import Icon from './icon';
 
 export default function Header(props) {
     return (
         <div className={styles.container}>
-            <p className={styles.chatTitle}>{(props.chat)?((props.chat.type == "GROUP")?(props.chat.name):("@" + props.chat.participants[0].name)):("Dashboard")}</p>
-            <Icon onClick={() => signOut({callbackUrl: '/..'})} image="/icons/account.png" imageDark="/icons/accountDark.png"/>
-            <Icon image="/icons/settings.png" imageDark="/icons/settingsDark.png"/>
+            <p className={styles.chatTitle}>{(props.chat)?((props.chat.type == "GROUP")?(props.chatName):("@"+props.chatName)):("Dashboard")}</p>
+            {props.children}
         </div>
     )
 }
---
+++ pages/api/password.js
@@ -0,0 +1,36 @@
+import prisma from "../../lib/prismadb";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./auth/[...nextauth]";
+var bcrypt = require('bcrypt');
+
+export default async function handler(req, res) {
+    const body = req.body;
+    const session = await getServerSession(req, res, authOptions);
+
+    const user = await prisma.user.findUnique({
+        where: {
+            email: session.user.email,
+        },
+    })
+
+    const isValid = await bcrypt.compare(body.oldPassword, user.password)
+  
+    if (!body.newPassword || !session) {
+        return res.status(400).json({ data: 'Missing data' });
+    } else if (!isValid) {
+        return res.status(400).json({ data: 'Wrong password' });
+    }
+    
+    bcrypt.hash(body.newPassword, 10, async function(err, hash) {
+        const userUpdate = await prisma.user.update({
+            where: {
+                email: session.user.email,
+            },
+            data: {
+                password: hash,
+            },
+        })
+    });
+  
+    res.status(200).json({ success: "success" });
+}
