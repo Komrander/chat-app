e6caf0c13aa943f3d0ead023045547b51ae25742
Matouš Pícl <87586480+picl-m@users.noreply.github.com>
2023-08-22 02:55:47 +0200

    Revert "Update direct.js"
    This reverts commit 3145c1a6f95bf0eb282a85d624270083f3b7c714.

--- pages/api/direct.js
+++ pages/api/direct.js
@@ -28,8 +28,6 @@ export default async function handler(req, res) {
         return res.status(400).json({ data: 'Chat already exist' })
     } else if (!user) {
         return res.status(400).json({ data: 'User doesnt exist' })
-    } else if (user.email == session.user.email)
-        return res.status(400).json({ data: 'Cannot message self' })
     }
 
     const chat = await prisma.chat.create({
